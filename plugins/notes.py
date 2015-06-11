e8db620c61d865177e79a9754c8eaaf9c99c47ed
dz <david@nodnod.net>
2015-06-11 18:59:37 -0400

    parse user info

--- plugins/notes.py
+++ plugins/notes.py
@@ -1,20 +1,21 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
+from itertools import chain
 import re
 
 AT_USER_MATCHER = re.compile(r'(\<@(\w+)\>)')
 
 NoteStore = {}
-UsersStore = {}
 
 def start_taking(userid):
     NoteStore[userid] = []
-    UsersStore[userid] = []
 
-def stop_taking(userid):
+def stop_taking(userid, client):
     notes = NoteStore.pop(userid, [])
-    users = UsersStore.pop(userid, [])
-    return "\n".join(str(note) for note in notes)
+    users = client.users
+    user_ids = list(chain.from_iterable([get_user_ids(note) for note in notes]))
+    import pdb; pdb.set_trace()
+    return "\n".join(replace_user_ids(note, users) for note in notes)
 
 def add_note(userid, note):
     NoteStore[userid].append(note)
@@ -28,7 +29,10 @@ def get_text(message):
 def user_is_taking_notes(userid):
     return userid in NoteStore
 
-def replace_user_values(text):
+def get_user_ids(text):
+    return [m[1] for m in AT_USER_MATCHER.findall(text)]
+
+def replace_user_ids(text):
     pass
 
 @respond_to('start')
@@ -47,7 +51,7 @@ def finish_notes(message):
     userid = get_user(message)
     text = get_text(message)
     if user_is_taking_notes(userid):
-        notes = stop_taking(userid)
+        notes = stop_taking(userid, message._client)
         message.reply("```%s```" % notes)
     else:
         message.reply('Error. Type `start` to start taking notes')
