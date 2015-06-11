5fd7a24216913bb1f42824ccb61075e735f161a5
dz <david@nodnod.net>
2015-06-11 18:41:10 -0400

    bot

--- bot.py
+++ bot.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python
-
 import sys
 import logging
 import logging.config
 from slackbot import settings
+from slackbot import dispatcher
 from slackbot.bot import Bot
+
+# monkey patch slackbot
+def new_filter(self, msg):
+    text = msg.get('text', '')
+    msg['text'] = text
+    return msg
+
+dispatcher.MessageDispatcher.filter_text = new_filter
 
 def main():
     kw = {
         'format': '[%(asctime)s] %(message)s',
         'datefmt': '%m/%d/%Y %H:%M:%S',
-        'level': logging.DEBUG if settings.DEBUG else logging.INFO,
+        'level': logging.DEBUG,
         'stream': sys.stdout,
     }
     logging.basicConfig(**kw)
-    logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARNING)
+    logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.DEBUG)
     bot = Bot()
     bot.run()
 
--- plugins/notes.py
+++ plugins/notes.py
@@ -1,13 +1,19 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
+import re
+
+AT_USER_MATCHER = re.compile(r'(\<@(\w+)\>)')
 
 NoteStore = {}
+UsersStore = {}
 
 def start_taking(userid):
     NoteStore[userid] = []
+    UsersStore[userid] = []
 
 def stop_taking(userid):
     notes = NoteStore.pop(userid, [])
+    users = UsersStore.pop(userid, [])
     return "\n".join(str(note) for note in notes)
 
 def add_note(userid, note):
@@ -21,6 +27,9 @@ def get_text(message):
 
 def user_is_taking_notes(userid):
     return userid in NoteStore
+
+def replace_user_values(text):
+    pass
 
 @respond_to('start')
 def start_notes(message):
@@ -44,9 +53,11 @@ def finish_notes(message):
         message.reply('Error. Type `start` to start taking notes')
 
 @respond_to('(.*)')
-def take_notes(message):
+def take_notes(message, something):
     userid = get_user(message)
     text = get_text(message)
+    if text == "finished" or text == "start":
+        return
     if user_is_taking_notes(userid):
         add_note(userid, text)
     else:
